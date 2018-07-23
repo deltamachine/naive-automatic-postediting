e493966fc599c08781f72c55f013da6c25d8b5c0
deltamachine <an-an-kondratjeva@yandex.ru>
2018-07-23 18:04:51 +0500

    draft of a new stuff

--- new_alg/extract_types.py
+++ new_alg/extract_types.py
@@ -2,8 +2,35 @@ import sys
 from collections import Counter
 
 
+def distance(a, b):
+    """Calculates the Levenshtein distance between a and b."""
+
+    n, m = len(a), len(b)
+
+    if n > m:
+        a, b = b, a
+        n, m = m, n
+
+    current_row = range(n + 1)
+
+    for i in range(1, m + 1):
+        previous_row, current_row = current_row, [i] + [0] * n
+
+        for j in range(1, n + 1):
+            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+            
+            if a[j - 1] != b[i - 1]:
+                change += 1
+
+            current_row[j] = min(add, delete, change)
+
+    return current_row[n]
+
+#приведи это говно в порядок, смотреть больно
 def find_bidix(postedits):
-	entries = []
+	bidix_entries = []
+	grammar_entries = []
+	other_entries = {}
 
 	for elem in postedits:
 		elem = elem.split('\',')
@@ -25,15 +52,68 @@ def find_bidix(postedits):
 		if len(source) == len(mt) and len(mt) == len(target):
 			for i in range(len(mt)):
 				if '*' in mt[i]:
-					entrie = '%s\t%s\t%s\t' % (source[i], mt[i], target[i])
-					entries.append(entrie)
+					bidix_entrie = '%s\t%s\t%s\t' % (source[i], mt[i], target[i])
+					bidix_entries.append(bidix_entrie) 
 
-	counter = Counter()
+					continue
 
-	for entrie in entries:
-		counter[entrie] += 1
+				dis = distance(mt[i], target[i])
+				letters = len(target[i])
 
-	print(counter.most_common(10))
+				if letters != 0:
+					edits_percent = ((letters - dis) / letters) * 100
+				else:
+					edits_percent = 0
+
+				if edits_percent >= 50 and edits_percent < 100:
+					grammar_entrie = '%s\t%s\t%s\t' % (source[i], mt[i], target[i])
+					grammar_entries.append(grammar_entrie)
+					#print(source)
+				elif mt[i] != target[i]: 
+					other_entrie = '%s\t%s\t%s\t' % (source[i], mt[i], target[i])
+					
+					if source[i] not in other_entries.keys():
+						other_entries[source[i]] = {}
+						other_entries[source[i]][mt[i]] = Counter()
+						other_entries[source[i]][mt[i]][target[i]] = 1
+					else:
+						if mt[i] not in other_entries[source[i]].keys():
+							other_entries[source[i]][mt[i]] = Counter()
+							other_entries[source[i]][mt[i]][target[i]] = 1
+						else:
+							if target[i] not in other_entries[source[i]][mt[i]].keys():
+								other_entries[source[i]][mt[i]][target[i]] = 1
+							else:
+								other_entries[source[i]][mt[i]][target[i]] += 1	
+				else:
+					continue	
+
+	bidix_counter = Counter()
+	grammar_counter = Counter()
+
+	for entrie in bidix_entries:
+		bidix_counter[entrie] += 1
+
+	for entrie in grammar_entries:
+		grammar_counter[entrie] += 1
+
+	with open('bidix_entries.txt', 'w', encoding='utf-8') as file:
+		for elem in bidix_counter.most_common():
+			file.write('%s%s\n' % (elem[0], str(elem[1])))
+
+	with open('grammar_entries.txt', 'w', encoding='utf-8') as file:
+		for elem in grammar_counter.most_common():
+			file.write('%s%s\n' % (elem[0], str(elem[1])))
+
+	for key, value in other_entries.items():
+		if len(value.keys()) > 1:
+			for sec_key, sec_value in value.items():
+				if len(sec_value.keys()) > 1:
+					v = sum(sec_value.values())
+					mc = sec_value.most_common(1)
+
+					if mc[0][1] * 100 / v > 50:
+						print(key, sec_key, mc[0][0], mc[0][1], v, mc[0][1] * 100 / v)
 
 
 def main():
